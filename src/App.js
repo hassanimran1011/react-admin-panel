bcc5f23063d83a0ec488e382e56ed54058240caa
hassanimran1011 <83642997+hassanimran1011@users.noreply.github.com>
2022-07-29 06:41:54 +0500

    too much updates

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import {TooltipComponent} from '@syncfusion/ej2-react-popups';
 
+import { Navbar, Footer, ThemeSettings, Sidebar } from './components';
+import { Calendar, ColorPicker, Customers, Ecommerce, Editor, Employees, Kanban, 
+        Orders, Area, Bar, ColorMapping, Financial, Line, Pie, Pyramid, Stacked } from './pages';
 import './App.css';
 
 const App = () => {
-  const activeMenu = false;
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
@@ -20,43 +23,43 @@ const App = () => {
           </div>
           {activeMenu ? (
           <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-            Sidebar enebled
+            <Sidebar />
           </div>
         ):(
           <div className='w-0 dark:bg-secondary-dark-bg'>
-            Sidebar disabled
+            <Sidebar />
           </div>
         )}
         <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
           <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-            Navbar
+            <Navbar />
           </div>
         </div>
         <div>
           <Routes>
             {/* Dashboards */}
             <Route path='/' element="home"/>
-            <Route path='/ecommerce' element="eCommerce"/>
+            <Route path='/ecommerce' element={<Ecommerce />}/>
             {/* Pages */}
-            <Route path='/orders' element="Orders"/>
-            <Route path='/employee' element="Employees"/>
-            <Route path='/products' element="Products"/>
-            <Route path='/customers' element="Customers"/>
+            <Route path='/orders' element={<Orders />}/>
+            <Route path='/employee' element={<Employees />}/>
+            <Route path='/customers' element={<Customers />}/>
 
             {/* Apps */}
-            <Route path='/ecommerce' element="eCommerce"/>
-            <Route path='/ecommerce' element="eCommerce"/>
-            <Route path='/ecommerce' element="eCommerce"/>
-            <Route path='/ecommerce' element="eCommerce"/>
+            <Route path='/kanban' element={<Kanban />}/>
+            <Route path='/editor' element={<Editor />}/>
+            <Route path='/calendar' element={<Calendar />}/>
+            <Route path='/color-picker' element={<ColorPicker />}/>
 
             {/* Charts */}
-            <Route path='/ecommerce' element="eCommerce"/>
-            <Route path='/ecommerce' element="eCommerce"/>
-            <Route path='/ecommerce' element="eCommerce"/>
-            <Route path='/ecommerce' element="eCommerce"/>
-            <Route path='/ecommerce' element="eCommerce"/>
-            <Route path='/ecommerce' element="eCommerce"/>
-            <Route path='/ecommerce' element="eCommerce"/>
+            <Route path='/line' element={<Line />}/>
+            <Route path='/area' element={<Area />}/>
+            <Route path='/bar' element={<Bar />}/>
+            <Route path='/pie' element={<Pie />}/>
+            <Route path='/financial' element={<Financial />}/>
+            <Route path='/color-mapping' element={<ColorMapping />} />
+            <Route path='/pyramid' element={<Pyramid />}/>
+            <Route path='/stacked' element={<Stacked />}/>
 
             
           </Routes>
